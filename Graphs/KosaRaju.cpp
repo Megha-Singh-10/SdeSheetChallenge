068e3df4060c965bfdf0267672a0c6a71bd15138
Megha-Singh-10 <megha.17.becs@acharya.ac.in>
2022-08-02 22:20:37 +0530

    Graph

---
+++ Graphs/KosaRaju.cpp
@@ -0,0 +1,19 @@
+void dfs(int node, stack<int> &st, vector<int> &vis, vector<int> adj[]) {
+    vis[node] = 1; 
+    for(auto it: adj[node]) {
+        if(!vis[it]) {
+            dfs(it, st, vis, adj); 
+        }
+    }
+    
+    st.push(node); 
+}
+void revDfs(int node, vector<int> &vis, vector<int> transpose[]) {
+    cout << node << " "; 
+    vis[node] = 1; 
+    for(auto it: transpose[node]) {
+        if(!vis[it]) {
+            revDfs(it, vis, transpose); 
+        }
+    }
+}
